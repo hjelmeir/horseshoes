da9a2d2949fd86bee5c921b3b76a8ed32be4e163
Clay Unicorn <thinkclay@gmail.com>
2019-06-05 16:26:09 -0400

    v0.0.14

--- src/helpers/resourceHelper.ts
+++ src/helpers/resourceHelper.ts
@@ -1,5 +1,100 @@
+import { union, merge } from "lodash";
 import { Resources, Resource } from "../models/resource";
 
 export function toArray<M extends Resource>(resources: Resources<M>): M[] {
   return resources.keys.map((key: string): M => resources.data[key]);
+}
+
+export function createResource<M extends Resource>(
+  state: Resources<M>,
+  payload?: M,
+  defaultResource?: M
+): Resources<M> {
+  if (!payload || !payload.key) {
+    console.log(
+      `Error in createResource, missing payload or key: Type: ${typeof payload}, Payload: ${payload}`
+    );
+    return state;
+  }
+
+  let newState = { ...state };
+  newState.keys = union(newState.keys, [payload.key]);
+  newState.data[payload.key] = merge(
+    newState.data[payload.key],
+    defaultResource,
+    payload
+  );
+
+  return newState as Resources<M>;
+}
+
+export function updateResource<M extends Resource>(
+  state: Resources<M>,
+  payload?: M
+): Resources<M> {
+  if (!payload || !payload.key) {
+    console.log(
+      `Error in updateResource, missing payload or key: Type: ${typeof payload}, Payload: ${payload}`
+    );
+    return state;
+  }
+
+  let newState = { ...state };
+  newState.keys = union(newState.keys, [payload.key]);
+  newState.data[payload.key] = merge(newState.data[payload.key], payload);
+
+  return newState as Resources<M>;
+}
+
+export function deleteResource<M extends Resource>(
+  state: Resources<M>,
+  payload?: M
+): Resources<M> {
+  if (!payload || !payload.key) {
+    console.log(
+      `Error in deleteResource, missing payload or key: Type: ${typeof payload}, Payload: ${payload}`
+    );
+    return state;
+  }
+
+  let newState = { ...state };
+  newState.keys = newState.keys.filter((key: string) => key !== payload.key);
+  delete newState.data[payload.key];
+
+  return newState as Resources<M>;
+}
+
+export function deleteResourcesBy<M extends Resource>(
+  matches: [string, string],
+  state: Resources<M>,
+  payload?: M
+): Resources<M> {
+  if (!payload || !payload.key) {
+    console.log(
+      `Error in deleteResourceBy, missing payload or key: Type: ${typeof payload}, Payload: ${payload}`
+    );
+    return state;
+  }
+
+  let newState = { ...state };
+  let dataArr = toArray<M>(state).filter(
+    record => record[matches[0]] === matches[1]
+  );
+  let dataKeys = dataArr.map(record => record.key);
+
+  if (dataArr.length === 0 || !dataArr[0].key) {
+    console.log(
+      `Error in deleteResourceBy, no matches found in state for: ${
+        matches[0]
+      }, ${matches[1]}`
+    );
+    return state;
+  }
+
+  newState.keys = newState.keys.filter(
+    (key: string) => !dataKeys.includes(key)
+  );
+  dataArr.forEach(record => delete newState.data[record.key]);
+
+  return newState as Resources<M>;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
-import { union, merge } from "lodash";
-import {
-  Resources,
-  Resource,
-  ResourcesAction,
-  ResourceAction
-} from "./models/resource";
-import { toArray } from "./helpers/resourceHelper";
+import * as model from "./models/resource";
+import * as helper from "./helpers/resourceHelper";
 
-export { Resources, Resource, ResourcesAction, ResourceAction, toArray };
+export { model, helper };
 
 export const defaultResources = {
   isLoading: false,
@@ -15,108 +9,11 @@ export const defaultResources = {
   data: {}
 };
 
-export function initResources<M extends Resource>(
-  data?: Resources<M>
-): Resources<M> {
+export function initResources<M extends model.Resource>(
+  data?: model.Resources<M>
+): model.Resources<M> {
   return {
     ...defaultResources,
     ...data
   };
-}
-
-//
-// Helpers
-//
-export function createResource<M extends Resource>(
-  state: Resources<M>,
-  payload?: M,
-  defaultResource?: M
-): Resources<M> {
-  if (!payload || !payload.key) {
-    console.log(
-      `Error in createResource, missing payload or key: Type: ${typeof payload}, Payload: ${payload}`
-    );
-    return state;
-  }
-
-  let newState = { ...state };
-  newState.keys = union(newState.keys, [payload.key]);
-  newState.data[payload.key] = merge(
-    newState.data[payload.key],
-    defaultResource,
-    payload
-  );
-
-  return newState as Resources<M>;
-}
-
-export function updateResource<M extends Resource>(
-  state: Resources<M>,
-  payload?: M
-): Resources<M> {
-  if (!payload || !payload.key) {
-    console.log(
-      `Error in updateResource, missing payload or key: Type: ${typeof payload}, Payload: ${payload}`
-    );
-    return state;
-  }
-
-  let newState = { ...state };
-  newState.keys = union(newState.keys, [payload.key]);
-  newState.data[payload.key] = merge(newState.data[payload.key], payload);
-
-  return newState as Resources<M>;
-}
-
-export function deleteResource<M extends Resource>(
-  state: Resources<M>,
-  payload?: M
-): Resources<M> {
-  if (!payload || !payload.key) {
-    console.log(
-      `Error in deleteResource, missing payload or key: Type: ${typeof payload}, Payload: ${payload}`
-    );
-    return state;
-  }
-
-  let newState = { ...state };
-  newState.keys = newState.keys.filter((key: string) => key !== payload.key);
-  delete newState.data[payload.key];
-
-  return newState as Resources<M>;
-}
-
-export function deleteResourcesBy<M extends Resource>(
-  matches: [string, string],
-  state: Resources<M>,
-  payload?: M
-): Resources<M> {
-  if (!payload || !payload.key) {
-    console.log(
-      `Error in deleteResourceBy, missing payload or key: Type: ${typeof payload}, Payload: ${payload}`
-    );
-    return state;
-  }
-
-  let newState = { ...state };
-  let dataArr = toArray<M>(state).filter(
-    record => record[matches[0]] === matches[1]
-  );
-  let dataKeys = dataArr.map(record => record.key);
-
-  if (dataArr.length === 0 || !dataArr[0].key) {
-    console.log(
-      `Error in deleteResourceBy, no matches found in state for: ${
-        matches[0]
-      }, ${matches[1]}`
-    );
-    return state;
-  }
-
-  newState.keys = newState.keys.filter(
-    (key: string) => !dataKeys.includes(key)
-  );
-  dataArr.forEach(record => delete newState.data[record.key]);
-
-  return newState as Resources<M>;
 }
